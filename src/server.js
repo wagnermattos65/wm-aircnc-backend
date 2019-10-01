5fcf9b1eda82a15ccfb11c0ab528998196fdbdfa
Wagner Mattos <wagner_mattos@msn.com>
2019-10-01 16:00:42 -0300

    feat: connect to database

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
+
 const app = express();
+mongoose.connect(process.env.MONGOBD_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
