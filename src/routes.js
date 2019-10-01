b33f4e01a6113a84084f99e0bc441b9c7dd38a49
Wagner Mattos <wagner_mattos@msn.com>
2019-10-01 18:14:11 -0300

    feat: create spots and set bookings

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/uploads')
+
 const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router();
+const upload = multer(uploadConfig)
 
 routes.get('/status',(req, res) => {
   res.json({ status: "Server is running", port: `${process.env.PORT}`, time:`${Date()}` })
 })
 
 routes.post('/sessions',SessionController.store)
+
+
+routes.get('/spots',SpotController.index)
+routes.post('/spots',upload.single('thumbnail'),SpotController.store)
+routes.post('/spots/:spot_id/bookings',BookingController.store)
+
+routes.get('/dashboard',DashboardController.show)
+
+
 
 module.exports = routes
