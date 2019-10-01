7275015e3ed1b13b554f5e4de1a76e67f04f9cf5
Wagner Mattos <wagner_mattos@msn.com>
2019-10-01 16:26:56 -0300

    feat: create&retrieve users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router();
 
 routes.get('/status',(req, res) => {
   res.json({ status: "Server is running", port: `${process.env.PORT}`, time:`${Date()}` })
 })
+
+routes.post('/sessions',SessionController.store)
 
 module.exports = routes
