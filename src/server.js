73fb757795a33ece9b97ba076ce834b9ca9747f8
Wagner Mattos <wagner_mattos@msn.com>
2019-10-02 17:51:59 -0300

    feat: create route to show thumbnails

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
+const path = require('path')
 
 
 const app = express();
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGOBD_CONNECTION_STRING, {
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')))
 app.use(routes)
 
 app.listen(process.env.PORT)
